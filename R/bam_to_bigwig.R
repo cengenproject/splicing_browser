7cc0042640337793fa85e7fde224e1f4f711eb84
AlexWeinreb <alexis.weinreb@yale.edu>
2021-08-19 00:48:03 -0400

    First try bam_to_bigwig

--- R/bam_to_bigwig.R
+++ R/bam_to_bigwig.R
@@ -1,20 +1,60 @@
-# Draft for visualization of raw alignments
+# Visualization of raw alignments: take in bams, make coverage (Rle) objects from them,
+# save the Rle as well as useful vizualisations: a mean per neuron type, and a min and max across
+# neuron type.
 
+
+# Initializations ----
 library(GenomicAlignments)
+library(tidyverse)
 
-awa <- coverage(readGAlignmentPairs("AWAr19.bam"))
-nsm <- coverage(readGAlignmentPairs("NSMr59.bam"))
 
-xx <- pmax(awa, nsm)
-pmin(awa, nsm)
-(awa + nsm)/2
+bam_dir <- "/home/aw853/scratch60/2021-08-18_alignments"
+output_dir <- "outputs_visualization"
 
-pryr::object_size(nsm)
-# 24,109,448 B  -> the Rle objects are small
+cat("Read in all files\n")
+all_covs <- tibble(path = list.files(bam_dir, pattern = "\\.bam$"),
+                   sample = stringr::str_split_fixed(path, "\\.", 2)[,1],
+                   neuron = stringr::str_split_fixed(sample, "r", 2)[,1],
+                   replicate = stringr::str_split_fixed(sample, "r", 2)[,2]) %>%
+  mutate(coverage = map(path, ~ coverage(readGAlignmentPairs(.x))))
 
-rtracklayer::export.bw(awa, "export_awa.bw")
-rtracklayer::export.bw(nsm, "export_NSM.bw")
-rtracklayer::export.bw(pmax(awa,nsm), "export_max.bw")
-rtracklayer::export.bw(pmin(awa,nsm), "export_min.bw")
-rtracklayer::export.bw((awa+nsm)/2, "export_mean.bw")
+cat("Write the single neurons\n")
+all_covs %>%
+  walk2(sample, coverage,
+        ~ rtracklayer::export.bw(object = .y,
+                                 con = file.path(output_dir, "single_neur", paste0(.x, ".bw"))))
+
+cat("Write the averages per neuron class\n")
+pmean <- function(list_of_covs){
+  reduce(list_of_covs, `+`) / length(list_of_covs)
+}
+
+reduced_cov <- all_covs %>%
+  group_by(neuron) %>%
+  summarize(mean_coverage = pmean(coverage))
+
+reduced_cov %>%
+  walk2(neuron, mean_coverage,
+        ~ rtracklayer::export.bw(object = .y,
+                                 con = file.path(output_dir, "means", paste0("mean_",.x, ".bw"))))
+
+
+cat("Write the global min, max, and average\n")
+reduced_cov %>%
+  pull(mean_coverage) %>%
+  pmin() %>%
+  rtracklayer::export.bw(file.path(output_dir, "global", "minimum.bw"))
+
+reduced_cov %>%
+  pull(mean_coverage) %>%
+  pmax() %>%
+  rtracklayer::export.bw(file.path(output_dir, "global", "maximum.bw"))
+
+reduced_cov %>%
+  pull(mean_coverage) %>%
+  pmean() %>%
+  rtracklayer::export.bw(file.path(output_dir, "global", "mean.bw"))
+
+
+cat("\n\nAll done.\n")
 
