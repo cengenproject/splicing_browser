fb9691b39090725a48bff3dda6160086ef05fee6
AlexWeinreb <alexis.weinreb@yale.edu>
2021-08-23 16:05:22 -0400

    bam to bigwig wrapper: 6 CPU, 80GB.

--- src/runR_bam_to_bigwig.sh
+++ src/runR_bam_to_bigwig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=general
 #SBATCH --job-name=R_bam_to_bigwig
-#SBATCH -c 1
-#SBATCH --mem=40G
+#SBATCH -c 6
+#SBATCH --mem=80G
 #SBATCH --time=1-18:10:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexis.weinreb@yale.edu
