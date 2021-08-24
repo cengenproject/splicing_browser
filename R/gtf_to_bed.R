91a81fea9e7ac5784037d2cf26980bac834dff03
AlexWeinreb <alexis.weinreb@yale.edu>
2021-08-24 10:33:13 -0400

    Add comments in gtf_to_bed

--- R/gtf_to_bed.R
+++ R/gtf_to_bed.R
@@ -19,8 +19,10 @@ wb_gtf <- rtracklayer::import(wb_get_gtf_path(277))
 
 
 ## Split into separate GRangesLists ----
+cat("split\n")
 grList_by_tx <- split(wb_gtf, wb_gtf$transcript_id)
 
+cat("filter GRangeLists\n")
 all_exons <- endoapply(grList_by_tx,
                        function(gr) gr[gr$type == "exon"])
 all_transcripts <- endoapply(grList_by_tx,
@@ -32,16 +34,19 @@ all_start_codons <- endoapply(grList_by_tx,
 all_stop_codons <- endoapply(grList_by_tx,
                              function(gr) gr[gr$type == "stop_codon"])
 
-# save(all_exons,all_transcripts,all_stop_codons,
-#      file = "data/tmp_grangelists_for_gtf2bigbed.rda")
+save(all_exons,all_transcripts,all_stop_codons,
+     file = "data/tmp_grangelists_for_gtf2bigbed.rda")
+
+
 
 ## Compute BED fields ----
-
+cat("Compute BED4\n")
 chrom <- vapply(seqnames(all_transcripts),
                 function(.x) as.character(.x@values), character(1L))
 chromStart <- as.integer(start(all_transcripts) -1)
 chromEnd <- as.integer(end(all_transcripts))
 name <- names(all_transcripts)
+cat("Compute BED9\n")
 score <- rep(1000, length(all_transcripts))
 strand <- vapply(strand(all_transcripts), 
                  function(.x) as.character(.x@values), character(1L))
@@ -50,6 +55,7 @@ thickStart <- as.integer(start(all_start_codons))
 thickEnd <- as.integer(end(all_stop_codons))
 itemRgb <- rep(".", length(all_transcripts))
 
+cat("Compute BED12\n")
 # block_counts <- vapply(all_exons, length, integer(1L))
 blockCount <- integer(length(all_transcripts))
 blockStarts <- character(length(all_transcripts))
@@ -71,14 +77,15 @@ for(cur_transcr in seq_along(all_exons)){
 
 
 ## Write to disk ----
-
+cat("Write to disk\n")
 readr::write_lines(paste(chrom, chromStart, chromEnd, name,score,strand,thickStart,
                          thickEnd,itemRgb,blockCount,blockSizes,blockStarts,
                          sep = "\t"),
                    "data/WS277_canonical_geneset.bed12")
 
-## For bigBed ----
 
+## For bigBed ----
+cat("Write chrom sizes\n")
 wb_fasta <- Biostrings::readDNAStringSet(wb_get_genome_path(277))
 wb_all_seqlengths <- data.frame(names(wb_fasta),
                                 Biostrings::width(wb_fasta))
