74b190f36bb6bb4c896c56e9c83a22e7e41905ad
AlexWeinreb <alexis.weinreb@yale.edu>
2021-08-24 16:39:10 -0400

    convert to bigBed after BED generated

--- src/runR_gtf_to_bed.sh
+++ src/runR_gtf_to_bed.sh
@@ -13,3 +13,13 @@ echo "---------------  Running R script gtf_to_bed.R ---------------------"
 module load R
 R --slave -f R/gtf_to_bed.R
 
+
+echo "---------------  Converting to bigBed ---------------------"
+out_dir="outputs_visualization/references"
+
+
+bedSort $out_dir/WS277_canonical_geneset.bed12 $out_dir/WS277_canonical_geneset.sorted.bed
+
+bedToBigBed $out_dir/WS277_canonical_geneset.sorted.bed \
+            $out_dir/chrom.WS277.sizes \
+            $out_dir/WS277_canonical_geneset.bb
