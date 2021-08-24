c5bb0e891b9300f32e57002896a02dabfa71ed11
AlexWeinreb <alexis.weinreb@yale.edu>
2021-08-24 15:21:46 -0400

    in gtf_to_bed, tackle case of spliced start/stop codons

--- R/gtf_to_bed.R
+++ R/gtf_to_bed.R
@@ -4,6 +4,7 @@ library(GenomicFeatures)
 ## Import ----
 wb_gtf <- rtracklayer::import(wb_get_gtf_path(277))
 
+wb_gtf <- wb_gtf[1:1000]
 
 ## // Not working: save with rtracklayer function ----
 
@@ -18,24 +19,33 @@ wb_gtf <- rtracklayer::import(wb_get_gtf_path(277))
 
 
 
-## Split into separate GRangesLists ----
-cat("split\n")
-grList_by_tx <- split(wb_gtf, wb_gtf$transcript_id)
+if(file.exists( "outputs_visualization/references/tmp_grangelists_for_gtf2bigbed.rda")){
+  cat("Loading cahed file...\n")
+  load("outputs_visualization/references/tmp_grangelists_for_gtf2bigbed.rda")
+} else{
+  ## Split into separate GRangesLists ----
+  cat("split\n")
+  grList_by_tx <- split(wb_gtf, wb_gtf$transcript_id)
+  
+  cat("filter GRangeLists\n")
+  all_exons <- endoapply(grList_by_tx,
+                         function(gr) gr[gr$type == "exon"])
+  all_transcripts <- endoapply(grList_by_tx,
+                               function(gr) gr[gr$type == "transcript"])
+  # all_UTR <- endoapply(grList_by_tx,
+  #                              \(gr) gr[gr$type %in% c("three_prime_utr","five_prime_utr")])
+  all_start_codons <- endoapply(grList_by_tx,
+                                function(gr) gr[gr$type == "start_codon"])
+  all_stop_codons <- endoapply(grList_by_tx,
+                               function(gr) gr[gr$type == "stop_codon"])
+  
+  save(all_exons,all_transcripts,all_stop_codons,all_start_codons,
+       file = "outputs_visualization/references/tmp_grangelists_for_gtf2bigbed.rda")
+  
+}
 
-cat("filter GRangeLists\n")
-all_exons <- endoapply(grList_by_tx,
-                       function(gr) gr[gr$type == "exon"])
-all_transcripts <- endoapply(grList_by_tx,
-                             function(gr) gr[gr$type == "transcript"])
-# all_UTR <- endoapply(grList_by_tx,
-#                              \(gr) gr[gr$type %in% c("three_prime_utr","five_prime_utr")])
-all_start_codons <- endoapply(grList_by_tx,
-                              function(gr) gr[gr$type == "start_codon"])
-all_stop_codons <- endoapply(grList_by_tx,
-                             function(gr) gr[gr$type == "stop_codon"])
 
-save(all_exons,all_transcripts,all_stop_codons,
-     file = "outputs_visualization/references/tmp_grangelists_for_gtf2bigbed.rda")
+
 
 
 
@@ -46,13 +56,42 @@ chrom <- vapply(seqnames(all_transcripts),
 chromStart <- as.integer(start(all_transcripts) -1)
 chromEnd <- as.integer(end(all_transcripts))
 name <- names(all_transcripts)
+
 cat("Compute BED9\n")
 score <- rep(1000, length(all_transcripts))
 strand <- vapply(strand(all_transcripts), 
                  function(.x) as.character(.x@values), character(1L))
 
-thickStart <- as.integer(start(all_start_codons))
-thickEnd <- as.integer(end(all_stop_codons))
+# start codons: problem when splicing within codon
+all_stcods <- start(all_start_codons)
+lengths_stcods <- vapply(all_stcods, length, integer(1L))
+
+spliced_stcods <- which(lengths_stcods == 2)
+for(stcod in spliced_stcods){
+  if(strand(all_start_codons[[stcod]])@values == "+"){
+    all_stcods[[stcod]] <- min(all_stcods[[stcod]])
+  } else{
+    all_stcods[[stcod]] <- max(all_stcods[[stcod]])
+  }
+}
+
+thickStart <- as.integer(all_stcods)
+
+# stop codons, same problem
+all_stcods <- end(all_stop_codons)
+lengths_stcods <- vapply(all_stcods, length, integer(1L))
+
+spliced_stcods <- which(lengths_stcods == 2)
+for(stcod in spliced_stcods){
+  if(strand(all_stop_codons[[stcod]])@values == "-"){
+    all_stcods[[stcod]] <- max(all_stcods[[stcod]])
+  } else{
+    all_stcods[[stcod]] <- min(all_stcods[[stcod]])
+  }
+}
+
+thickEnd <- as.integer(all_stcods)
+
 itemRgb <- rep(".", length(all_transcripts))
 
 cat("Compute BED12\n")
