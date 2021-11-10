d25803e895e91eda9907c8e4ac7b0a3586e0f462
AlexWeinreb <alexis.weinreb@yale.edu>
2021-11-09 19:57:49 -0500

    Implement Rle_utils for parallel computation of percentile etc of coverage.

--- R/Rle_utils.R
+++ R/Rle_utils.R
@@ -1,12 +1,80 @@
 
 
-# pmean: parallel mean of a group of Rle.
-# The main function is implemented in C++
+
+
+# For tests
+test_list_of_SimpleRleList <- list(
+  RleList(c(I = Rle(c(1,1,1,1,8,8,8,2,2,2,2)),
+            II = Rle(c(2,2,2,5,5,5,2,2,2,2,3))), compress = FALSE),
+  RleList(c(I = Rle(c(2,2,2,5,5,5,2,2,2,2,3)),
+            II = Rle(c(1,1,1,1,8,8,8,2,2,2,2))), compress = FALSE)
+)
+
+direct_quantile <- function(x, i) sort(x)[1+floor(length(x)*i)]
+
+
+# Expect a list of SimpleRleList, where each has one Rle per chromosome.
+# Function .f must accept a vector of integer values and return a single int.
+# Note this is very slow and is only to use for testing small lists.
+apply_per_position <- function(list_of_rlelist, .f, ...){
+  # check number of chromosomes
+  nb_chrom <- purrr::map_int(list_of_rlelist, length)
+  nb_chrom <- unique(nb_chrom)
+  if(length(nb_chrom) != 1) stop("Not all RleLists have the same number of chromosomes")
+  
+  out <- vector("list", length = nb_chrom)
+  for(cur_chrom in seq_len(nb_chrom)){
+    expanded_vecs <- purrr::map(list_of_rlelist,
+                         ~ rep(.x[[cur_chrom]]@values, .x[[cur_chrom]]@lengths))
+    
+    chrom_length <- purrr::map_int(expanded_vecs, length)
+    chrom_length <- unique(chrom_length)
+    if(length(chrom_length) != 1) stop("The chromosome lengths differ.")
+    
+    
+    chrom_as_mat <- matrix(unlist(expanded_vecs),
+                           byrow = TRUE,
+                           nrow = length(list_of_rlelist),
+                           ncol = chrom_length)
+    out[[cur_chrom]] <- Rle(apply(chrom_as_mat, 2, .f, ..., simplify = TRUE))
+  }
+  out <- RleList(out)
+  names(out) <- names(list_of_rlelist[[1]])
+  out
+}
+
+
 Rcpp::sourceCpp("src/Rle_utils.cpp")
 
-pmean <- function(...) cpmean(list(...))
 
 
+
+# Note: pmean is as or more efficient as R implementation,
+# but good for tests
+
+# # pmean: parallel mean of a group of Rle.
+# # The main function is implemented in C++
+# 
+# pmean <- function(...) cpmean(list(...))
+# 
+# 
+# # Support functions to call on every chromosome in a simpleRleList
+# # Copied from S4Vectors (v 0.30.0)
+# 
+# setMethod("pmean", "Rle", function(...)
+#   S4Vectors:::.psummary.Rle(pmean, ...))
+# 
+# 
+# setMethod("pmean", "RleList", function(...)
+#   mendoapply(pmean, ...))
+
+
+
+
+pmean <- function(...) {
+  ll <- list(...)
+  Reduce(`+`, ll)/length(ll)
+}
 
 # Support functions to call on every chromosome in a simpleRleList
 # Copied from S4Vectors (v 0.30.0)
@@ -17,4 +85,76 @@ setMethod("pmean", "Rle", function(...)
 
 setMethod("pmean", "RleList", function(...)
   mendoapply(pmean, ...))
+
+# Test!
+stopifnot(all.equal(unlist(do.call(pmean,test_list_of_SimpleRleList)),
+                    suppressWarnings(unlist(apply_per_position(test_list_of_SimpleRleList, mean)))))
+
+
+
+
+# pmedian ----
+pmedian <- function(...) cpmedian(list(...))
+setMethod("pmedian", "Rle", function(...)
+  S4Vectors:::.psummary.Rle(pmedian, ...))
+
+
+setMethod("pmedian", "RleList", function(...)
+  mendoapply(pmedian, ...))
+
+
+
+# Test!
+stopifnot(all.equal(unlist(do.call(pmedian,test_list_of_SimpleRleList)),
+                    suppressWarnings(unlist(apply_per_position(test_list_of_SimpleRleList, median)))))
+
+
+
+
+# p lower percentile ----
+plower <- function(...) c_low_percentile(list(...))
+setMethod("plower", "Rle", function(...)
+  S4Vectors:::.psummary.Rle(plower, ...))
+
+
+setMethod("plower", "RleList", function(...)
+  mendoapply(plower, ...))
+
+
+
+
+# Test!
+stopifnot(all.equal(unlist(do.call(plower,
+                                   test_list_of_SimpleRleList)),
+                    suppressWarnings(unlist(apply_per_position(test_list_of_SimpleRleList,
+                                                               direct_quantile, .1)))))
+
+
+
+
+
+# p higher percentile ----
+phigher <- function(...) c_high_percentile(list(...))
+setMethod("phigher", "Rle", function(...)
+  S4Vectors:::.psummary.Rle(phigher, ...))
+
+
+setMethod("phigher", "RleList", function(...)
+  mendoapply(phigher, ...))
+
+
+
+
+# Test!
+stopifnot(all.equal(unlist(do.call(phigher,
+                                   test_list_of_SimpleRleList)),
+                    suppressWarnings(unlist(apply_per_position(test_list_of_SimpleRleList,
+                                                               direct_quantile, .9)))))
+
+# bench::mark(do.call(phigher,
+#                     test_list_of_SimpleRleList),
+# apply_per_position(test_list_of_SimpleRleList,
+#                    direct_quantile, .9),
+# check = FALSE)
+
 
--- src/Rle_utils.cpp
+++ src/Rle_utils.cpp
@@ -2,8 +2,23 @@
 using namespace Rcpp;
 
 
-// Parallel mean of a list of vectors the same length
+/*** R
+# For basic testing
+test_list <- list(1:5, 2:6, 7:3)
 
+p_apply <- function(my_list, .f, ...){
+  stopifnot(dplyr::n_distinct(sapply(my_list, length)) == 1)
+  
+  mat <- matrix(unlist(my_list), byrow = TRUE, nrow = length(my_list))
+  
+  apply(mat, 2, .f, ...)
+}
+*/
+
+
+
+
+// Parallel mean of a list of vectors the same length (for tests)
 // [[Rcpp::export]]
 NumericVector cpmean(List x) {
   int nb=x.size();
@@ -36,5 +51,191 @@ NumericVector cpmean(List x) {
 
 
 /*** R
-timesTwo(42)
+stopifnot(all.equal(cpmean(test_list),
+                    Reduce(`+`, test_list)/length(test_list)))
+stopifnot(all.equal(cpmean(test_list),
+                    p_apply(test_list, mean)))
 */
+
+
+
+
+
+
+// Implement parallel median
+// [[Rcpp::export]]
+NumericVector cpmedian(List x) {
+  int nb=x.size();
+  NumericVector x0=x[0];
+  int n=x0.size();
+  
+  NumericMatrix mat(nb,n);
+  
+  //fill in matrix
+  for(int i=0;i<nb;i++){
+    NumericVector cur_x = x[i];
+    
+    for(int j=0;j<n;j++){
+      mat(i,j) = cur_x[j];
+    }
+  }
+  
+  //Apply median to each column
+  NumericVector out(n);
+  for(int j=0; j<n; j++){
+    NumericVector cur_col = mat(_,j);
+    out[j] = median(cur_col);
+  }
+  return out;
+}
+
+/*** R
+stopifnot(all.equal(cpmedian(test_list),
+                    p_apply(test_list, median)))
+*/
+
+
+
+// Implement parallel percentiles
+
+// first, percentile for 1 vector
+double percentile(NumericVector x, int nth){
+  
+  std::nth_element (x.begin(), x.begin()+nth-1, x.end());
+  
+  return x[nth-1];
+}
+
+// Use it for list, first with median to double check
+// [[Rcpp::export]]
+NumericVector cpmedian2(List x) {
+  int nb=x.size();
+  NumericVector x0=x[0];
+  int n=x0.size();
+  
+  NumericMatrix mat(nb,n);
+  
+  //fill in matrix
+  for(int i=0;i<nb;i++){
+    NumericVector cur_x = x[i];
+    
+    for(int j=0;j<n;j++){
+      mat(i,j) = cur_x[j];
+    }
+  }
+  
+  //Apply median to each column
+  
+  int nth = 1+nb/2;
+  NumericVector out(n);
+  for(int j=0; j<n; j++){
+    NumericVector cur_col = mat(_,j);
+    out[j] = percentile(cur_col, nth);
+  }
+  return out;
+}
+
+/*** R
+stopifnot(all.equal(cpmedian(test_list),
+                    cpmedian2(test_list)))
+test_list2 <- list(c( 1, 1, 2, 2, 2),
+                   c(12,12, 1, 7, 8),
+                   c( 2, 2, 3, 3, 3),
+                   c(10,10,12,12,12),
+                   c(10,11,12,13,14))
+
+stopifnot(all.equal(cpmedian(test_list2),
+                    cpmedian2(test_list2)))
+*/
+
+
+// then 10th percentile
+// [[Rcpp::export]]
+NumericVector c_low_percentile(List x) {
+  int nb=x.size();
+  NumericVector x0=x[0];
+  int n=x0.size();
+  
+  NumericMatrix mat(nb,n);
+  
+  //fill in matrix
+  for(int i=0;i<nb;i++){
+    NumericVector cur_x = x[i];
+    
+    for(int j=0;j<n;j++){
+      mat(i,j) = cur_x[j];
+    }
+  }
+  
+  //Apply median to each column
+  
+  int nth = 1+nb/10;
+  NumericVector out(n);
+  for(int j=0; j<n; j++){
+    NumericVector cur_col = mat(_,j);
+    out[j] = percentile(cur_col, nth);
+  }
+  return out;
+}
+
+/*** R
+direct_quantile <- function(x, i) sort(x)[1+floor(length(x)*i)]
+stopifnot(all.equal(c_low_percentile(test_list),
+                    p_apply(test_list, direct_quantile, .1)))
+test_list2 <- list(c( 1, 1, 2, 2, 2),
+                   c(12,12, 1, 7, 8),
+                   c( 2, 2, 3, 3, 3),
+                   c(10,10,12,12,12),
+                   c(10,11,12,13,14))
+
+stopifnot(all.equal(c_low_percentile(test_list2),
+                    p_apply(test_list2, direct_quantile, .1)))
+*/
+
+
+
+
+// and 90th percentile
+// [[Rcpp::export]]
+NumericVector c_high_percentile(List x) {
+  int nb=x.size();
+  NumericVector x0=x[0];
+  int n=x0.size();
+  
+  NumericMatrix mat(nb,n);
+  
+  //fill in matrix
+  for(int i=0;i<nb;i++){
+    NumericVector cur_x = x[i];
+    
+    for(int j=0;j<n;j++){
+      mat(i,j) = cur_x[j];
+    }
+  }
+  
+  //Apply median to each column
+  
+  int nth = 1+9*nb/10;
+  NumericVector out(n);
+  for(int j=0; j<n; j++){
+    NumericVector cur_col = mat(_,j);
+    out[j] = percentile(cur_col, nth);
+  }
+  return out;
+}
+
+/*** R
+stopifnot(all.equal(c_high_percentile(test_list),
+                    p_apply(test_list, direct_quantile, .9)))
+test_list2 <- list(c( 1, 1, 2, 2, 2),
+                   c(12,12, 1, 7, 8),
+                   c( 2, 2, 3, 3, 3),
+                   c(10,10,12,12,12),
+                   c(10,11,12,13,14))
+
+stopifnot(all.equal(c_high_percentile(test_list2),
+                    p_apply(test_list2, direct_quantile, .9)))
+*/
+
+
+
